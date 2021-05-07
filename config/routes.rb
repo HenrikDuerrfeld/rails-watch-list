55e86934d321752d087ac29e1edc24fa0dbdbab0
Henrik Duerrfeld <henrik.duerrfeld.20002@gmail.com>
2021-05-07 23:55:59 +0200

    Prepare rails app with external specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: "lists#index"
-    resources :lists, only: [:new, :create, :show] do
+    resources :lists, only: [:new, :create, :index, :show] do
         resources :bookmarks, only: [:new, :create, :destroy]
       end
 end
